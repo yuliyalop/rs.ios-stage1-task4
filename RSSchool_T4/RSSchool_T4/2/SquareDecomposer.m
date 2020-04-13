9b49b3e6cd7650bd1809eb024aaab351c8ff47cc
yuliya <penguindobro@gmail.com>
2020-04-13 23:00:04 +0300

    feat: task 2 done

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,26 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
-}
+     NSMutableArray<NSNumber*> *result = [NSMutableArray arrayWithObject: number];
+
+        long summ = 0;
+        while ([result count] != 0) {
+            long point = [result lastObject].longValue;
+            [result removeLastObject];
+            summ += pow(point, 2);
+
+            for (long i = point - 1; i > 0; i--) {
+                if (summ - (pow(i, 2)) >= 0) {
+                    summ -= pow(i, 2);
+                    [result addObject: @(i)];
+                    if (summ == 0) {
+                        return [result sortedArrayUsingSelector: @selector(compare:)];
+                    }
+                }
+            }
+
+        }
+
+        return nil;
+    }
 @end
